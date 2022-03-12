6b9d8a40d7aab36c2fe039e62477d12f0d6b2827
seifezz <seifezz2001@gmail.com>
2022-03-12 16:39:13 +0200

    ex1
    first exercise in ACM

---
+++ Project1/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long n;
+	long long x;
+	long long y;
+	long long profit; 
+	cin >> n >> x >> y;
+		profit = (y * n) - (x * n);
+		cout << profit << "\n";
+
+
+
+
+
+
+		return 0;
+  }
